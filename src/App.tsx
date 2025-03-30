96e1c32d553d97678761440e9193f93c7d3981b3
mani <ganoglamanikanta@gmail.com>
2025-03-30 19:29:15 +0530

    The Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,32 @@
-import {createBrowserRouter, RouterProvider } from 'react-router-dom'
-import SignUp from './signUp'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-import Home from './Dashboard/home'
+import Home from './Dashboard/home.tsx'
 import Neet from './Neet/neet.tsx'
 import Eapcet from './Eapcet/eapcet.tsx'
 import Cuet from './Cuet/cuet.tsx'
-import Questions from './questions/question'
-import Neet2 from './Neet/neet2'
+import Questions from './questions/question.tsx'
+import Neet2 from './Neet/neet2.tsx'
 import Score from './score'
-import Eapcet2 from './Eapcet/Eapcet2'
-import Cuet2 from './Cuet/Cuet2'
+import Eapcet2 from './Eapcet/Eapcet2.tsx'
+import Cuet2 from './Cuet/Cuet2.tsx'
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path:"/login",
-      element:<SignUp/>
-    },
-    {
-      path:'/home',
-      element:<Home/>
-    }
-    ,{
-      path:'/neet',
-      element:<Neet/>
-    },
-    {
-      path:'/eapcet',
-      element:<Eapcet/>
-    },
-    {
-      path:'/cuet',
-      element:<Cuet/>
-    },{
-      path:'/question',
-      element:<Questions/>
-    },{
-      path:'/neet2',
-      element:<Neet2/>
-    },{
-      path:'/score',
-      element:<Score/>
-    },{
-      path:'/eapcet2',
-      element:<Eapcet2/>
-    },{
-      path:'/cuet2',
-      element:<Cuet2/>
-    },{
-      path:'/score',
-      element:<Score/>
-    }
+  const router = createHashRouter([
+    { path: '/home', element: <Home /> },
+    { path: '/neet', element: <Neet /> },
+    { path: '/eapcet', element: <Eapcet /> },
+    { path: '/cuet', element: <Cuet /> },
+    { path: '/question', element: <Questions /> },
+    { path: '/neet2', element: <Neet2 /> },
+    { path: '/score', element: <Score /> },
+    { path: '/eapcet2', element: <Eapcet2 /> },
+    { path: '/cuet2', element: <Cuet2 /> }
   ])
+
   return (
-    <>
-     <main>
-      <RouterProvider router={router}></RouterProvider>
-     </main>
-    </>
+    <main>
+      <RouterProvider router={router} />
+    </main>
   )
 }
 
